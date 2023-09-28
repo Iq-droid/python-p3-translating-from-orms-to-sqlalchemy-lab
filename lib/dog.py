6c5dde28101ecf1690157edd21a288fe874de1cc
Iqra Ali <iqra.ali@student.moringaschool.com>
2023-09-28 23:59:44 +0300

    sqlalchemy

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
 
 def create_table(base):
+    base.metadata.create_all()
     pass
 
 def save(session, dog):
+    session.add(dog)
+    session.commit()
     pass
 
 def get_all(session):
+    session.query(Dog).all()
     pass
 
 def find_by_name(session, name):
+    return session.query(Dog).filter(Dog.name == name).first()
     pass
 
 def find_by_id(session, id):
+    return session.query(Dog).filter(Dog.id == id).first()
     pass
 
 def find_by_name_and_breed(session, name, breed):
+    return session.query(Dog).filter(Dog.name == name, Dog.breed).first()
     pass
 
 def update_breed(session, dog, breed):
+    dog.breed = breed
+    session.commit()
     pass
